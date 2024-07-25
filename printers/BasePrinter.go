169f7d5387e4f855595fc93690723005fafae889
jolang <jolang@jolang.org>
2024-07-25 14:34:01 +0200

    indent

--- printers/BasePrinter.go
+++ printers/BasePrinter.go
@@ -16,19 +16,18 @@ func NewBasePrinter() *BasePrinter {
 }
 
 func (p *BasePrinter) Write(b []byte) (n int, err error) {
+	p.PrintIndent()
 	p.Buffer += string(b)
 	return len(b), nil
 }
 
 func (p *BasePrinter) Printf(format string, a ...any) {
+	p.PrintIndent()
 	_, _ = fmt.Fprintf(p, format, a...)
 }
 
 func (p *BasePrinter) Print(strs ...string) {
-	if p.ShouldPrintIndent {
-		p.PrintIndent()
-		p.ShouldPrintIndent = false
-	}
+	p.PrintIndent()
 	for i, s := range strs {
 		if i == 0 {
 			p.Buffer += s
@@ -44,8 +43,13 @@ func (p *BasePrinter) Println(s ...string) {
 	p.ShouldPrintIndent = true
 }
 
-func (u *BasePrinter) PrintIndent() {
-	for i := 0; i < u.Indent; i++ {
-		u.Buffer += "\t"
+func (p *BasePrinter) PrintIndent() {
+	if !p.ShouldPrintIndent {
+		return
+	}
+
+	p.ShouldPrintIndent = false
+	for i := 0; i < p.Indent; i++ {
+		p.Buffer += "\t"
 	}
 }
